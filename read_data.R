df3fec2abd55797411aa906f27e872373a663d1e
Gabriel Kroktov <GKrotkov@users.noreply.github.com>
2023-09-26 13:25:11 -0400

    adjusting variable names
    I swear this is the final commit for this r script

--- read_data.R
+++ read_data.R
@@ -11,13 +11,17 @@ houses$no_baths <- houses$no_full_baths + (0.5 * houses$no_half_baths)
 
 houses$zip <- factor(houses$zip)
 
-houses$bedgroup <- factor(houses$bedrooms)
+houses$bedrooms <- factor(houses$bedrooms)
 
 houses <- houses[, c("acre", "adj2007", "bedrooms", "bedgroup", "bikescore", 
                      "walkscore", "distance", "garage_spaces", "no_rooms", 
                      "no_baths", "squarefeet", "zip")]
 
-fit <- lm(log(adj2007) ~ log(distance) + bedgroup + garage_spaces + no_baths + 
+colnames(houses) <- c("acre", "home_value", "bedrooms", "bedgroup", "bikescore", 
+                      "walkscore", "distance", "garage_spaces", "n_rooms", 
+                      "n_baths", "squarefeet", "zip")
+
+fit <- lm(log(home_value) ~ log(distance) + bedgroup + garage_spaces + n_baths + 
               zip + squarefeet, data = houses)
 
 summary(fit)
