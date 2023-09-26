8bb2523af8ec1cd080273647e7f4f69bbe7ef907
Gabriel Kroktov <GKrotkov@users.noreply.github.com>
2023-09-26 12:20:31 -0400

    Add files via upload

--- read_data.R
+++ read_data.R
@@ -11,14 +11,19 @@ houses$no_baths <- houses$no_full_baths + (0.5 * houses$no_half_baths)
 
 houses$zip <- factor(houses$zip)
 
-houses$bedgroup <- factor(houses$bedgroup, 
-                          levels = c("1-2 beds", "3 beds", "4+ beds"))
+houses$bedgroup <- factor(ifelse(houses$bedrooms %in% c(1), "1", 
+                                 ifelse(houses$bedrooms %in% c(2, 3, 4), 
+                                        "2-4", "5+")), 
+                          levels = c("1", "2-4", "5+"))
 
 houses <- houses[, c("acre", "adj2007", "bedrooms", "bedgroup", "bikescore", 
                      "walkscore", "distance", "garage_spaces", "no_rooms", 
                      "no_baths", "squarefeet", "zip")]
 
-fit <- lm(log(adj2007) ~ log(distance) + bedgroup + garage_spaces + no_baths + zip + 
-               squarefeet, data = houses)
+fit <- lm(log(adj2007) ~ log(distance) + bedgroup + garage_spaces + no_baths + 
+              zip + squarefeet, data = houses)
+
+summary(fit)
+plot(fit)
 
 save(houses, fit, file = "data.rda")
