c180485d1921fd04b52ef5255ff9ff8fb861451f
Gabriel Kroktov <GKrotkov@users.noreply.github.com>
2023-09-27 12:20:49 -0400

    commit before linear models

--- read_data.R
+++ read_data.R
@@ -16,12 +16,12 @@ houses$garage_spaces <- factor(ifelse(houses$garage_spaces > 1,
                                       "2+", houses$garage_spaces))
 
 houses <- houses[, c("acre", "adj2007", "bedrooms", "bedgroup", "bikescore", 
-                     "walkscore", "distance", "garage_spaces", "no_rooms", 
-                     "n_baths", "squarefeet", "zip")]
+                     "walkscore", "distance", "distgroup", "garage_spaces", 
+                     "no_rooms", "n_baths", "squarefeet", "zip")]
 
 colnames(houses) <- c("acre", "home_value", "bedrooms", "bedgroup", "bikescore", 
-                      "walkscore", "distance", "garage_spaces", "n_rooms", 
-                      "n_baths", "squarefeet", "zip")
+                      "walkscore", "distance", "distgroup", "garage_spaces", 
+                      "n_rooms", "n_baths", "squarefeet", "zip")
 
 fit <- lm(log(home_value) ~ log(distance) + bedrooms + garage_spaces + n_baths + 
               zip + squarefeet, data = houses)
