3ec7020c732f36dfeb275b2db39de773f1e2444a
Gabriel Kroktov <GKrotkov@users.noreply.github.com>
2023-09-27 09:24:55 -0400

    naming correction

--- read_data.R
+++ read_data.R
@@ -7,7 +7,7 @@ library(tidyverse)
 houses <- suppressMessages(read_csv("JSE13-070R2.csv"))
 
 # for simplicity, combine full and half baths
-houses$no_baths <- houses$no_full_baths + (0.5 * houses$no_half_baths)
+houses$n_baths <- houses$no_full_baths + (0.5 * houses$no_half_baths)
 
 houses$zip <- factor(houses$zip)
 
@@ -18,7 +18,7 @@ houses$garage_spaces <- factor(ifelse(houses$garage_spaces > 1,
 
 houses <- houses[, c("acre", "adj2007", "bedrooms", "bedgroup", "bikescore", 
                      "walkscore", "distance", "garage_spaces", "no_rooms", 
-                     "no_baths", "squarefeet", "zip")]
+                     "n_baths", "squarefeet", "zip")]
 
 colnames(houses) <- c("acre", "home_value", "bedrooms", "bedgroup", "bikescore", 
                       "walkscore", "distance", "garage_spaces", "n_rooms", 
@@ -28,6 +28,5 @@ fit <- lm(log(home_value) ~ log(distance) + bedrooms + garage_spaces + n_baths +
               zip + squarefeet, data = houses)
 
 summary(fit)
-plot(fit)
 
 save(houses, fit, file = "data.rda")
